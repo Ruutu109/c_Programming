2422b425d2c9c0d1570a992690f35768ab16f998
Ruutu109 <jadhavruturaj1668@gmail.com>
2024-09-20 15:04:10 +0530

    Basic Programs

---
+++ 1st.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n Hello G");
+
+    getch();
+    return 0;
+}
---
+++ Multiplication of User Defined Numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int num;
+    int num2;
+
+    printf("Enter First Number : ");
+    scanf("%d",&num);
+    printf("Enter Second Number : ");
+    scanf("%d",&num2);
+
+    printf("Multiplication of Two Numbers is %d \n", num*num2);
+
+    getch();
+    return 0;
+}
