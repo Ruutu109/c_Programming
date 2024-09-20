48a41439cdfa1ac85ffa664a61109a13aec4a4fc
Ruutu109 <jadhavruturaj1668@gmail.com>
2024-09-20 15:24:34 +0530

    Assignment

---
+++ Basic/Division.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int num =4;
+    int num2 =3;
+
+    printf("Division of Numbers is %d \n", num/num2);
+
+    getch();
+    return 0;
+}
