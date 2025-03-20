87205d6588c33b70ef1bccbea3dd9b42976b5310
Ruturaj Jadhav <jadhavruturaj1668@gmail.com>
2025-03-20 12:29:22 +0530

    Assignment
    Assignment

---
+++ Assignment/Assi 1 On Variables, Data Types , Input Output/Q6. Print Size of int, float,etc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("Size of int: %d bytes\n", sizeof(int));
+    printf("Size of float: %d bytes\n", sizeof(float));
+    printf("Size of double: %d bytes\n", sizeof(double));
+    printf("Size of char: %d byte\n", sizeof(char));
+
+    getch();
+    return 0;
+}
